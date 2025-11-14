dabd1628c3a2b978ad82ef51d5509b00185c4daa
A27403 <your-email@example.com>
2025-11-14 11:40:54 +0900

    your message

--- prisma/migrations/20251113025705_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Lesson" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "jp" TEXT NOT NULL,
-    "romaji" TEXT NOT NULL,
-    "np" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
-    "type" TEXT NOT NULL
-);
